dfe1f327cb9037684898f0f4b78badb0ee2bd4a8
Александра <shigaleva04@inbox.ru>
2024-03-05 16:11:42 +0300

    Добавьте файлы проекта.

---
+++ Praktika_clock/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Praktika_clock
+{
+    partial class Clock
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Clock));
+            this.pictureBox_clock = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_clock)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox_clock
+            // 
+            this.pictureBox_clock.BackColor = System.Drawing.Color.MidnightBlue;
+            this.pictureBox_clock.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_clock.Image")));
+            this.pictureBox_clock.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox_clock.Name = "pictureBox_clock";
+            this.pictureBox_clock.Size = new System.Drawing.Size(350, 350);
+            this.pictureBox_clock.TabIndex = 0;
+            this.pictureBox_clock.TabStop = false;
+            this.pictureBox_clock.Click += new System.EventHandler(this.pictureBox_clock_Click);
+            // 
+            // Clock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 350);
+            this.Controls.Add(this.pictureBox_clock);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(350, 350);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(350, 350);
+            this.Name = "Clock";
+            this.Text = "Clock";
+            this.Load += new System.EventHandler(this.Clock_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_clock)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_clock;
+    }
+}
+
---
+++ Praktika_clock/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Praktika_clock
+{
+    public partial class Clock : Form
+    {
+        //объявление глобальных переменных
+        int center_x = 175,       //центр циферблата
+            center_y = 175,
+
+            second_arrow = 100,   //длина секундной стрелки
+            minut_arrow = 90,     //длина минутной стрелки
+            hour_arrow = 75;      //длина часовой стрелки
+
+        Timer timer_clock = new Timer();    //создаем таймер
+
+        public Clock()
+        {
+            InitializeComponent();
+
+            //настройка прозрачности окна
+            this.BackColor = Color.MidnightBlue;
+            this.TransparencyKey = this.BackColor;
+        }
+
+        //Функция запуска таймера
+        private void Clock_Load(object sender, EventArgs e)
+        {
+            timer_clock.Interval = 1000;
+            timer_clock.Tick += new EventHandler(this.timer_clock_Tick);
+            timer_clock.Start();
+        }
+
+        //Функция получения координат конца секундной и минутной стрелок
+        private int[] Get_arrow_sm_coordination(int amount_second /*кол-во сек*/, int length_arrow /*длина стрелки*/)
+        {
+            int[] arrow_coord = new int[2];
+            amount_second *= 6;   //1 сек - 6 градусов (360/60)
+
+            //определение в какой четверти окружности находится конец стрелки
+            if (amount_second >= 0 && amount_second <= 180)
+            {
+                arrow_coord[0] = center_x + (int)(length_arrow * Math.Sin(Math.PI * amount_second / 180));
+                arrow_coord[1] = center_y - (int)(length_arrow * Math.Cos(Math.PI * amount_second / 180));
+            }
+            else
+            {
+                arrow_coord[0] = center_x - (int)(length_arrow * -Math.Sin(Math.PI * amount_second / 180));
+                arrow_coord[1] = center_y - (int)(length_arrow * Math.Cos(Math.PI * amount_second / 180));
+            }
+
+            return arrow_coord;
+        }
+
+        //Функция получения координат конца часовой стрелки
+        private int[] Get_arrow_h_coordination(int amount_minut /*кол-во мин*/, 
+            int amount_hour /*кол-во мин*/, int length_arrow /*длина стрелки*/)
+        {
+            int[] arrow_coord = new int[2];
+            int amount_time = (int)((length_arrow * 30) + (amount_minut * 0.5));  //1 час - 30 градусов, 1 минута - 0.5 градусов
+
+            //определение в какой четверти окружности находится конец стрелки
+            if (amount_time >= 0 && amount_time <= 180)
+            {
+                arrow_coord[0] = center_x + (int)(length_arrow * Math.Sin(Math.PI * amount_time / 180));
+                arrow_coord[1] = center_y - (int)(length_arrow * Math.Cos(Math.PI * amount_time / 180));
+            }
+            else
+            {
+                arrow_coord[0] = center_x - (int)(length_arrow * -Math.Sin(Math.PI * amount_time / 180));
+                arrow_coord[1] = center_y - (int)(length_arrow * Math.Cos(Math.PI * amount_time / 180));
+            }
+
+            return arrow_coord;
+        }
+
+        //Функция отрисовки стрелок на циферблате
+        private void timer_clock_Tick(object sender, EventArgs e)
+        {
+            //считываем системное время
+            int amount_second = DateTime.Now.Second;
+            int amount_minut = DateTime.Now.Minute;
+            int amount_hour = DateTime.Now.Hour;
+
+            int[] arrow_coord = new int[2];
+
+            Graphics graph = pictureBox_clock.CreateGraphics();
+
+            //Стираем предыдущее положения секундной стрелки
+            arrow_coord = Get_arrow_sm_coordination(amount_second, second_arrow + 4);
+            graph.DrawLine(new Pen(Color.White, 45f), new Point(center_x, center_y), new Point(arrow_coord[0], arrow_coord[1]));
+
+            // Стираем предыдущее положение минутной стрелки
+            arrow_coord = Get_arrow_sm_coordination(amount_minut, minut_arrow + 4);
+            graph.DrawLine(new Pen(Color.White, 40f), new Point(center_x, center_y), new Point(arrow_coord[0], arrow_coord[1]));
+
+            // Стираем предыдущее положение часовой стрелки
+            arrow_coord = Get_arrow_h_coordination(amount_hour % 12, amount_minut, hour_arrow + 4);
+            graph.DrawLine(new Pen(Color.White, 20f), new Point(center_x, center_y), new Point(arrow_coord[0], arrow_coord[1]));
+
+            /*----------------------------------------------------------------------------------------------------------------------*/
+
+            //Отрисовка часовой стрелки
+            arrow_coord = Get_arrow_h_coordination(amount_hour % 12, amount_minut, hour_arrow);
+            graph.DrawLine(new Pen(Color.Gray, 4f), new Point(center_x, center_y), new Point(arrow_coord[0], arrow_coord[1]));
+
+            // Отрисовка минутной стрелки
+            arrow_coord = Get_arrow_sm_coordination(amount_minut, minut_arrow);
+            graph.DrawLine(new Pen(Color.Black, 2f), new Point(center_x, center_y), new Point(arrow_coord[0], arrow_coord[1]));
+
+            // Отрисовка секундной стрелки.
+            arrow_coord = Get_arrow_sm_coordination(amount_second, second_arrow);
+            graph.DrawLine(new Pen(Color.DarkOrange, 2f), new Point(center_x, center_y), new Point(arrow_coord[0], arrow_coord[1]));  
+
+        }
+
+        //Закрытие рабочего окна
+        private void pictureBox_clock_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
